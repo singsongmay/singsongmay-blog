36d1790ea622acf6963a8c1c96bbf16ee8fd40dc
singsongmay <149139480+singsongmay@users.noreply.github.com>
2023-10-30 19:57:45 +0900

    Update index.tsx

--- src/routes/Feed/PostList/index.tsx
+++ src/routes/Feed/PostList/index.tsx
@@ -54,7 +54,7 @@ const PostList: React.FC<Props> = ({ q }) => {
     <>
       <div className="my-2">
         {!filteredPosts.length && (
-          <p className="text-gray-500 dark:text-gray-300">Nothing! 😺</p>
+          <p className="text-gray-500 dark:text-gray-300">검색 결과가 없어요! 🙃</p>
         )}
         {filteredPosts.map((post) => (
           <PostCard key={post.id} data={post} />
