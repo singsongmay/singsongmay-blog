de2f41970953330c91da25397954bc393d89c755
singsongmay <149139480+singsongmay@users.noreply.github.com>
2023-10-30 19:52:07 +0900

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        🌟 사이트
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
